56cea7cb3174e68016c5ea581ffd865762d74484
Mamta Kumari <alok@spageo.co.in>
2026-08-12 19:13:54 +0530

    Phase 2.3.3: Define model configuration component interface

---
+++ ui/model_config.py
@@ -0,0 +1,8 @@
+from qgis.PyQt.QtWidgets import QWidget
+
+
+class ModelConfigWidget(QWidget):
+    """SPAGeo model configuration UI component."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
