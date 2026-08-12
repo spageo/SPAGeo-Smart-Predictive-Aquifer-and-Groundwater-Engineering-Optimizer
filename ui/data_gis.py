0b39af6dea2731a15b9f5bb4960a8a8778d9d7d4
Mamta Kumari <alok@spageo.co.in>
2026-08-12 21:48:35 +0530

    Phase 2.3.4: Define Data and GIS component interface

---
+++ ui/data_gis.py
@@ -0,0 +1,8 @@
+from qgis.PyQt.QtWidgets import QWidget
+
+
+class DataGISWidget(QWidget):
+    """SPAGeo data and GIS UI component."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
