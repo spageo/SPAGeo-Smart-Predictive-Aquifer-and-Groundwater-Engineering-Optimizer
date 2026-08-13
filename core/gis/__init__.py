bf3b357526b1a299960c1d0297b1f8fa3f8e76e6
Mamta Kumari <alok@spageo.co.in>
2026-08-13 19:19:38 +0530

    Phase 2.4: Establish GIS core interface boundary

---
+++ core/gis/__init__.py
@@ -0,0 +1,9 @@
+"""SPAGeo GIS core interfaces.
+
+This package defines the architectural boundary between
+the SPAGeo application layer and future GIS and spatial
+data implementations.
+
+Concrete GIS implementations are introduced in
+their respective development phases.
+"""
